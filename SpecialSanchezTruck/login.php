da5f1b7270e74daf7b0e09fcebe397af0222901f
sanchezasix <sanchezsanzvgk@gmail7.com>
2018-11-08 18:16:23 +0100

    sprint2

--- SpecialSanchezTruck/login.php
+++ SpecialSanchezTruck/login.php
@@ -6,8 +6,32 @@
 
 <?php
 
-echo ("<p>Nombre de usuario: ".$_REQUEST["nombreusuario"]."</p>");
-echo ("<p>Nombre: ".$_REQUEST["nombre"]."</p>");
+$usuaris=["admin","user"];
+
+$user=$_REQUEST["inputUser"];
+$passw=$_REQUEST["inputPassword"];
+$recordar=$_REQUEST["recuerdame"];
+
+if ($passw=="1234" && in_array($user, $usuaris)) {
+
+    $_SESSION['username']=$user;
+    
+    if ($user=="admin")
+        $_SESSION['role']="admin";
+    else if ($user=="user")
+        $_SESSION["role"]="user";
+
+    if ($recordar=="recuerdame"){
+
+        setcookie('SstUser', $_SESSION['username'], time() + 365 * 24 * 60 * 60);
+        setcookie('SstUser', $_SESSION['role'], time() + 365 * 24 * 60 * 60);
+    
+    }
+
+}
+
+header("Location: index.php")
+
 
 
 ?>
--- SpecialSanchezTruck/loginForm.php
+++ SpecialSanchezTruck/loginForm.php
@@ -28,13 +28,13 @@
             <div class="card card-container">
                 <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
                 <p id="profile-name" class="profile-name-card"></p>
-                <form action="datoslogin.php" class="form-signin" method="get">
+                <form action="login.php" class="form-signin" method="get">
                     <span id="reauth-email" class="reauth-email"></span>
                     <input type="text" name="inputUser" class="form-control" placeholder="Usuario" required autofocus>
                     <input type="password" name="inputPassword" class="form-control" placeholder="Contraseña" required>
                     <div id="remember" class="checkbox">
                         <label>
-                            <input type="checkbox" value="remember-me"> Recordarme
+                            <input type="checkbox" value="recuerdame" name="recuerdame"> Recordarme
                         </label>
                     </div>
                     <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Acceder</button>
