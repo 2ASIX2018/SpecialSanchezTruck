bf51a8b10aa4f051578199fc432066420ac5422d
sanchezasix <sanchezsanzvgk@gmail7.com>
2018-11-29 12:49:32 +0100

    sprint3

--- SpecialSanchezTruck/datosregistro.php
+++ SpecialSanchezTruck/datosregistro.php
@@ -1,26 +1,26 @@
-<html>
-
-<body>
-
-<h1>Datos del Formulario de Registro.</h1>
-
 <?php
 
-echo ("<p>Nombre de usuario: ".$_REQUEST["nombreusuario"]."</p>");
-echo ("<p>Nombre: ".$_REQUEST["nombre"]."</p>");
-echo ("<p>Apellidos: ".$_REQUEST["apellidos"]."</p>");
-echo ("<p>Password introducido: ".$_REQUEST["password"]."</p>");
-echo ("<p>Email: ".$_REQUEST["email"]."</p>");
-echo ("<p>Direccion: ".$_REQUEST["direccion"]."</p>");
-echo ("<p>DNI: ".$_REQUEST["dni"]."</p>");
-echo ("<p>Telefono: ".$_REQUEST["telefono"]."</p>");
-echo ("<p>Ciudad: ".$_REQUEST["ciutat"]."</p>");
-echo ("<p>Pais: ".$_REQUEST["pais"]."</p>");
-echo ("<p>Provincia: ".$_REQUEST["provincia"]."</p>");
-echo ("<p>CD postal: ".$_REQUEST["codpostal"]."</p>");
+require("models/usuari.php");
+
+$unam=$_REQUEST["nombreusuario"];
+$nom=$_REQUEST["nombre"];
+$apll=$_REQUEST["apellidos"];
+$passw=$_REQUEST["password"];
+$email=$_REQUEST["email"];
+$direc=$_REQUEST["direccion"];
+$dni=$_REQUEST["dni"];
+$tlf=$_REQUEST["telefono"];
+$cit=$_REQUEST["ciutat"];
+$pais=$_REQUEST["pais"];
+$prov=$_REQUEST["provincia"];
+$cod=$_REQUEST["codpostal"];
+$rol="user";
+
+
+$articulo=new Usuari();
+$articulo->altaUsuari($unam, $nom, $apll, $email, $passw, $direc, $dni, $tlf, $cit, $cod, $pais, $prov, $rol);
+
+#header("Location: index.php");
+
 
 ?>
-
-
-</body>
-</html>
--- SpecialSanchezTruck/models/usuari.php
+++ SpecialSanchezTruck/models/usuari.php
@@ -28,6 +28,67 @@
                 $db=null;
                 }
         }
+
+        public function altaUsuari($unam, $nom, $apll, $email, $passw, $direc, $dni, $tlf, $cit, $cod, $pais, $prov, $rol){
+
+            try{
+                
+                require_once("conexio.php");
+                
+                $cadenaConnexio="mysql:host=".$connexio["servidor"].";dbname=".$connexio['bd'];
+                $dbCon = new PDO($cadenaConnexio, $connexio["usuari"], $connexio["contrassenya"]); 
+                $dbCon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);     
+                #echo '555555555';
+                echo $cadenaConnexio;
+
+                $consulta = $dbCon->prepare('INSERT INTO `Usuarios`
+                (`username`,
+                `nombre`,
+                `apellidos`,
+                `email`,
+                `passwd`,
+                `direccion`,
+                `dni`,
+                `telefono`,
+                `poblacion`,
+                `cod_postal`,
+                `pais`,
+                `provincia`,
+                `rol`)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);');
+                #mysql_query($consulta);
+                #echo '5555555';
+
+                echo("<br/>Unombre:".$unam);
+                echo("<br/>Nombre:".$nom);
+                echo("<br/>Apellido".$apll);
+                echo("<br/>nimagen".$email);
+                echo("<br/>Contraseña".$passw);
+                echo("<br/>Direccio:".$direc);
+                echo("<br/>DNI:".$dni);
+                echo("<br/>Telefono:".$tlf);
+                echo("<br/>Ciudad:".$cit);
+                echo("<br/>Pais:".$pais);
+                echo("<br/>CP:".$cod);
+                echo("<br/>Prov:".$prov);
+                echo("<br/>Rol:".$rol);
+                
+
+                
+                $consulta->execute(array($unam, $nom, $apll, $email, $passw, $direc, $dni, $tlf, $cit, $cod, $pais, $prov, $rol));
+                #echo '6666666';
+                #echo $consulta;
+                print_r($dbCon->errorInfo());
+                echo("Files afectades: ".$consulta->rowCount());
+                            
+                $dbCon=null;
+                
+            } catch (PDOException $e){
+                echo("Error:".$e->getMessage());
+                $dbCon=null;
+            }
+        }
+
     }   
 ?>
 
