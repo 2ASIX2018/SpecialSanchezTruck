ff5a0a93dafb153d257766ae82cf2174f370e574
sanchezasix <sanchezsanzvgk@gmail7.com>
2018-11-29 13:27:17 +0100

    sprint3

--- SpecialSanchezTruck/index.php
+++ SpecialSanchezTruck/index.php
@@ -199,6 +199,28 @@
           <div class="card h-100">
             <a><img class="card-img-top" src="<?php echo( $articulos[$i]["nimagen"]);?>"><a>
           <!--a href="#"><img class="card-img-top" src="imagenes/pagprincipal/scania_weeda.jpg" alt=""></a-->
+
+          <?php
+          if ($user!="Anonimo"){
+            ?>
+          <div>  
+          <p class="clasificacion">
+            <input id="radio1" type="radio" name="estrellas" value="5">
+            <label for="radio1">★</label>
+            <input id="radio2" type="radio" name="estrellas" value="4">
+            <label for="radio2">★</label>
+            <input id="radio3" type="radio" name="estrellas" value="3">
+            <label for="radio3">★</label>
+            <input id="radio4" type="radio" name="estrellas" value="2">
+            <label for="radio4">★</label>
+            <input id="radio5" type="radio" name="estrellas" value="1">
+            <label for="radio5">★</label>
+          </p>
+          </div>
+          <?php
+          }
+          ?>
+          
             <div class="card-body">
               <h4 class="card-title"> <?php echo( $articulos[$i]["nombre"]); ?></h4>
               <p class="card-text"><?php echo ($articulos[$i]["descripcion"]); ?></p>
